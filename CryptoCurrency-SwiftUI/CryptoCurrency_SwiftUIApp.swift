ea9fe687730c55b5faef1a6c2102d1fa0356f2ba
melikesoygullucu <melikesoygul@hotmail.com>
2024-08-28 15:30:45 +0300

    Initial Commit

---
+++ CryptoCurrency-SwiftUI/CryptoCurrency_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrency_SwiftUIApp.swift
+//  CryptoCurrency-SwiftUI
+//
+//  Created by Melike Soygüllücü on 28.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrency_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
