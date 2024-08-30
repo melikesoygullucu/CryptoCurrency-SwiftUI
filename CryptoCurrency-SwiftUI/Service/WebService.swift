3d7715643a8fda45851bf16ce4a06de05879b2a7
melikesoygullucu <melikesoygul@hotmail.com>
2024-08-30 23:39:25 +0300

    Web service added.

---
+++ CryptoCurrency-SwiftUI/Service/WebService.swift
@@ -0,0 +1,14 @@
+//
+//  WebService.swift
+//  CryptoCurrency-SwiftUI
+//
+//  Created by Melike Soygüllücü on 30.08.2024.
+//
+
+import Foundation
+
+class WebService {
+    func getCurrencies() {
+        
+    }
+}
